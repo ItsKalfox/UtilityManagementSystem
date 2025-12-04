68c1fe1928ae651e039e62d2a83d9bbd7b8ad1e1
Nipun Wijethunga <nmwijethunga@students.nsbm.ac.lk>
2025-12-04 18:49:10 +0530

    Initial project structure setup

---
+++ src/main/java/com/UtilityManagementSystem/UtilityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.UtilityManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UtilityManagementSystem/UtilityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UtilityManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
